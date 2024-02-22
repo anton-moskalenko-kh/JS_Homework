53df2bba70e6e8cb43778d312ecfd3bcbc295502
anton-moskalenko-kh <anton.moskalenko.kh@gmail.com>
2024-02-22 11:39:53 +0200

    JS_HW_Lesson1

---
+++ Lesson1/main.js
@@ -0,0 +1,41 @@
+// Task 1
+
+let a = 'hello';
+let b = 'owu';
+let c = 'com';
+let d = 'ua';
+let e = 1;
+let f = 10;
+let g = -999;
+let h = 123;
+let i = 3.14;
+let j = 2.7;
+let k = 16;
+let l = true;
+let m = false;
+
+console.log(a,b,c,d,e,f,g,h,i,j,k,l,m)
+
+// Task 2
+
+let firstName = 'Anton';
+let middleName = 'Romanovich';
+let lastName = 'Moskalenko'
+
+let fullName = `${firstName} ${middleName} ${lastName}`;
+console.log(fullName)
+
+// Task 3
+
+let first = 100;
+let second = '100';
+let third = true;
+
+console.log(typeof first, typeof second, typeof third);
+
+// Task 4
+let name = prompt('Enter your name');
+let fatherName = prompt('Enter your father name');
+let age = +prompt('Enter your age');
+let myFullName = `My name is ${name} ${fatherName}. I'm ${age} years old`
+console.log(myFullName);
